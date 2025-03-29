34d3d1290e93b37ace7a9bba9846a8199ea1365c
Alex22Simi <simionescualexandru22@gmail.com>
2025-03-29 10:33:37 +0000

    add: HelloWorld

---
+++ HelloWorld/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
+//!!impotant
+//limbaj compilat:
+//are nevoie de o masina virtuala
+
+//limbaj interpretat:
+//se interpreteaza linie cu linie
